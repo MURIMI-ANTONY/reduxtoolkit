f557d41a30fb3510c94f9efbcedaed912e26b9f1
Murimi <amurimi101@gmail.com>
2024-12-10 14:42:01 +0300

    TypeScript reinforced

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -3,7 +3,7 @@ import {login,logout} from "../store";
 import  {useDispatch,useSelector} from "react-redux";
 
 function Login(){
-    const [newUserName, setNewUserName] = useState("");
+    const [newUserName, setNewUserName] = useState<string>("");
     const dispatch = useDispatch();
     const username = useSelector((state:any)=>state.user.value.username)
     return(
@@ -11,8 +11,9 @@ function Login(){
             <h1>This is the Login Page</h1>
             {username}
             <br></br>
+            {/* // React.ChangeEvent<HTMLInputElement></HTMLInputElement> ...typescript knows e is a React vhange event from an input element */}
             <input type="text" 
-            onChange={(e)=>{
+            onChange={(e:React.ChangeEvent<HTMLInputElement>)=>{
                 setNewUserName(e.target.value);
             }}></input>
             <button type="submit" onClick={()=>dispatch(login({username: newUserName}))}>Login</button>
--- src/store.ts
+++ src/store.ts
@@ -1,12 +1,19 @@
-import { configureStore,createSlice } from "@reduxjs/toolkit";
+import { configureStore,createSlice, PayloadAction } from "@reduxjs/toolkit";
 
-const initialState = { value : {username:""}};
+
+interface UserStateValue {
+    username: string;
+}
+interface UserState{
+    value:UserStateValue;
+}
+const initialState = { value : {username:""}} as UserState;
 
 const userSlice = createSlice({
     name : "user",
     initialState,
     reducers:{
-        login:(state,action)=>{
+        login:(state:UserState,action:PayloadAction<UserStateValue)=>{
             state.value = action.payload;
         },
         logout:(state)=>{
